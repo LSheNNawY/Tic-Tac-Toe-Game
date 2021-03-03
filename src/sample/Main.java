4e7e7bfed9b5b763bd66fde6167a01f15dc1fc4b
LSheNNawY <devshennawy@gmail.com>
2021-03-03 17:16:31 +0200

    modifying Main class for changing scene

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    static Scene scene;
+    static Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        stage = primaryStage;
+        scene = new Scene(loadFXML("main"), 643, 687);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    static void setSceneSize(double width, double height) {
+        stage.setHeight(height);
+        stage.setWidth(width);
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
     public static void main(String[] args) {
         launch(args);
