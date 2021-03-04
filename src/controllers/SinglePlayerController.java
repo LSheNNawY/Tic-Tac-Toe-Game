51010e7872199ba707651c4d59add0648be742b0
LSheNNawY <devshennawy@gmail.com>
2021-03-04 03:19:04 +0200

    add back action

--- src/controllers/SinglePlayerController.java
+++ src/controllers/SinglePlayerController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -10,6 +14,7 @@ public class SinglePlayerController {
 
     public Text playerOneScore;
     public Text playerTwoScore;
+    public Button backBtn;
     @FXML
     private ResourceBundle resources;
 
@@ -23,5 +28,9 @@ public class SinglePlayerController {
     void initialize() {
         assert singleRoot != null : "fx:id=\"singleRoot\" was not injected: check your FXML file 'singlePlayer.fxml'.";
 
+    }
+
+    public void backAction(ActionEvent event) throws IOException {
+        Main.setRoot("main");
     }
 }
